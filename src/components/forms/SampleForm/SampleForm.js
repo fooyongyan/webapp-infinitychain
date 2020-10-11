f52c7da630d1ddfa809dd54d793277a05909e1a5
fooyongyan <fooyongyan@gmail.com>
2020-10-11 21:16:09 +0800

    Added Formik tok the mix to handle for submission within React Apps

--- src/components/forms/SampleForm/SampleForm.js
+++ src/components/forms/SampleForm/SampleForm.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import Field from '../Field/Field';
+import Field from '../components/Field/Field';
+import TextArea from '../components/TextArea/TextArea';
+import Selection from '../components/Selection/Selection';
 
 export default function SampleForm ( props ) {
 
     return (
-        <div style = {{width: "300px", margin: "auto"}}> 
+        <div style = {{width: "400px", margin: "auto"}}> 
             <Field label = "Name" type="text"/>
             <Field label = "Email" type="email"/>
             <Field label = "Location" type="text"/>
             <Field label = "Salary" type = "number"/>
             <Field label = "Password" type = "password"/>
             <Field label = "Confirm Pass" type = "password"/>
+            <Field label = "Attachment" type = "file"/>
+            <TextArea label = "Description" validation = "error" rows={10}/>
+            <Selection label = "Options" />
         </div>
     )
 }
