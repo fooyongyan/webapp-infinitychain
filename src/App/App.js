5b39de29527c1c73d821667ff45480f169450dc0
P@ndarism <fooyongyan@gmail.com>
2020-10-12 08:41:34 +0800

    Bug fixes

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Year from '../components/calendar/Year/Year';
 import SampleForm from '../components/forms/SampleForm/SampleForm'
 import './App.scss';
 function App() {
