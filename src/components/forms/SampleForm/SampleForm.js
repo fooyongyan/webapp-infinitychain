437311b1df1ec558b40400468e4743b1463f8631
P@ndarism <fooyongyan@gmail.com>
2020-10-12 08:57:38 +0800

    Added Forms for Submission

--- src/components/forms/SampleForm/SampleForm.js
+++ src/components/forms/SampleForm/SampleForm.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Field from '../components/Field/Field';
 import TextArea from '../components/TextArea/TextArea';
 import Selection from '../components/Selection/Selection';
-
+import Button from '../components/Button/Button';
+import './SampleForm.scss'
 export default function SampleForm ( props ) {
 
     return (
         <div style = {{width: "400px", margin: "auto"}}> 
-            <Field label = "Name" type="text"/>
+            <p> Hello, Please fill in the forms accordingly</p>
+            <Field label = "Name" type="text" name="name"/>
             <Field label = "Email" type="email"/>
             <Field label = "Location" type="text"/>
             <Field label = "Salary" type = "number"/>
@@ -16,6 +18,10 @@ export default function SampleForm ( props ) {
             <Field label = "Attachment" type = "file"/>
             <TextArea label = "Description" validation = "error" rows={10}/>
             <Selection label = "Options" />
+            <div className = "Commands"> 
+                <Button type = "Primary"> Submit </Button>
+                <Button type = "Secondary"> Cancel </Button>
+            </div>
         </div>
     )
 }
