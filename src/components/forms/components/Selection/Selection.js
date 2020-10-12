d9339454c6fc114153146154ff1f4b848ba79b55
P@ndarism <fooyongyan@gmail.com>
2020-10-12 08:41:21 +0800

    Added function checks to determine initial form value submission

--- src/components/forms/components/Selection/Selection.js
+++ src/components/forms/components/Selection/Selection.js
@@ -2,13 +2,18 @@ import React from 'react'
 import '../components.scss'
 
 export default function Selection(props) {
-    const options = props.options ? props.options : [ {value: "lime", label: "Lime"}];
+    const options = props.options ? props.options : [ 
+        {value: "lime", label: "Lime"},
+        {value: "lemon", label: "Lemon"},
+        {value: "apple", label: "Apple"},
+    ];
+    const selected = props.selected ? props.selected : "lime"
 
     return (
         <div className = "Field"> 
             <label> {props.label} </label>
             <select>
-                {options.map( e =>  <option value={e.value}>{e.label}</option>)}
+                {options.map( e =>  <option value={e.value} selected = {e.value === selected}>{e.label}</option>)}
             </select>
         </div>
     )
